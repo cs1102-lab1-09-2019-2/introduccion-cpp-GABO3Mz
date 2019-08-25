94434ba92c8bee0131e3a0d699583b59fde4da30
GABO3M <gabriel.espinoza@utec.edu.pe>
2019-08-25 13:38:13 -0500

    Ejercicio 3 terminado

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using std::cout;
+using std::endl;
 
 int main() {
-    for (int i = 0; i < ; i++) {
+    int i;
+    for (int i = 0; i < 10; i++) {
         //imprimir los 10 numeros [0-9]
+        cout << i << endl;
     }
+    return 0;
 }
