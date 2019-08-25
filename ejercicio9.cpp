f4675449df5be3582394028e4da840a512af2cc2
GABO3M <gabriel.espinoza@utec.edu.pe>
2019-08-25 15:16:48 -0500

    Ejercicio 9 terminado

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using std::cout;
+using std::endl;
 
 int main() {
 
     int i = 1;
     while (i <= 10) {
-        if (   ) {
+        if (i%2==0) {
             //imprimir numeros pares verticalmente
+            cout<<i<<endl;
         }
+        i++;
 
     }
+    return 0;
 }
